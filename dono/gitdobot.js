f39ec6165b9467abf3778b77618722e9ca85204a
Your Name <you@example.com>
2025-05-15 15:08:58 -0300

    first commit

---
+++ dono/gitdobot.js
@@ -0,0 +1,5 @@
+const gitdobot = (prefix) => {
+return`*Oiiiiêê, esta versão minha só está disponível na bronxys> _dash.bronxyshost.com_ o melhor site de hospedagem de bots/sites da América latina. nao perca tempo, tenha agora os melhores bots super atualizados do mundoooo*🤩🤩🤩`
+}
+
+exports.gitdobot = gitdobot
---
+++ scheyot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+while : 
+do
+echo "${GREEN}
+
+✘ OLÁ  SOU A SAKURA-V7.5 ^-^
+(⁠｡⁠•̀ᴗ⁠-⁠)✧ INICIANDO MEUS SISTEMAS⚡${WHITE}"
+if [ "$1" = "bronxys" ]; then
+node connect.js sim
+else 
+node connect.js
+fi
+
+sleep 1
+done
